c7851594d1b349b78616db9a18d2735e37f3cf2f
Victor Radael <victorradael+github@gmail.com>
2025-03-17 12:09:16 -0300

    feature: :sparkles: Loading component created

---
+++ components/Loading.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+
+const LoadingScreen = () => {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color="#078be3" />
+            <Text style={styles.text}>Carregando...</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5F5F5',
+    },
+    text: {
+        marginTop: 10,
+        fontSize: 16,
+        color: '#333',
+    },
+});
+
+export default LoadingScreen;
