1752791170962a1cf5a539d886ec84a6c0f49071
Victor Radael <victorradael+github@gmail.com>
2025-03-18 16:09:52 -0300

    feature: :sparkles: Bills registration created

--- app/(app)/(tabs)/bills.tsx
+++ app/(app)/(tabs)/bills.tsx
@@ -1,10 +1,101 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import DropDownModal from '@/components/DropDownModal';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, Button, Snackbar } from 'react-native-paper';
+import { DatePickerInput } from 'react-native-paper-dates';
+import { createExpense } from '@/api/requests';
 
-export default function BillsScreen() {
+
+export default function SignIn() {
+  const [category, setCategorty] = useState<number | string>();
+  const [billValue, setBillValue] = useState<string>();
+  const [date, setDate] = useState<Date>();
+  const [message, setMessage] = useState("");
+  const [visible, setVisible] = useState(false);
+  const categories = ['Luz', 'Condomínio', 'Fundo de Reserva', 'Agua Área Comum', 'Água', 'Gás', 'Outros'];
+
+  const formatCurrency = (text: string) => {
+
+    let cleanValue = text.replace(/\D/g, "");
+
+    let numericValue = parseFloat(cleanValue) / 100;
+
+    return numericValue.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  };
+
+  const handleChangeText = (text: string) => {
+    let formatted = formatCurrency(text);
+    setBillValue(formatted);
+  };
+
+  const registerNewBill = async () => {
+    if (!category || !billValue || !date) {
+      setMessage("Por favor, preencha todos os campos!");
+      setVisible(true);
+      return;
+    }
+
+    try {
+      await createExpense({
+        description: String(category),
+        amount: Number(billValue.replace("R$", "").replace(",", ".")),
+        date: date?.toISOString(),
+      });
+
+      setMessage("Despesa registrada com sucesso! ✅");
+    } catch (error) {
+      setMessage("Erro ao registrar a despesa! ❌");
+    } finally {
+      setVisible(true);
+    }
+  };
+
+
+
+  const getSelectedOption = () => category;
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>About Screen</Text>
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+
+      <DropDownModal getSelectedOption={getSelectedOption} options={categories} setSelectedOption={setCategorty} label='Categoria' />
+      <View style={styles.inputContaier}>
+        <TextInput
+          label="Valor"
+          value={billValue}
+          onChangeText={handleChangeText}
+
+
+
+        />
+      </View>
+      <View style={styles.inputContaier}>
+        <DatePickerInput
+          locale="pt-BR"
+          label="Data"
+          value={date}
+          onChange={(d) => setDate(d)}
+          inputMode="start"
+
+        />
+      </View>
+
+      {category && billValue && date &&
+        <Button mode="text" style={styles.button} onPress={() => { registerNewBill() }}>
+          Adicionar
+        </Button>
+      }
+      <Snackbar
+        visible={visible}
+        onDismiss={() => setVisible(false)}
+        duration={3000}
+      >
+        {message}
+      </Snackbar>
+    </GestureHandlerRootView>
   );
 }
 
@@ -12,10 +103,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f3faff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
-  text: {
-    color: '#3e4756',
+  inputContaier: {
+    marginBottom: 30,
   },
+  button: {
+    marginTop: 20,
+    borderRadius: 8,
+  }
 });
